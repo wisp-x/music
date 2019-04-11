b89e2820cb1f3c0ddfe8fba964647bf7ca18e2e2
Wisp X <1591788658@qq.com>
2019-04-11 16:36:44 +0800

    :sparkles: 完成首页基本排版

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,20 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Music',
+      home: MyHomePage(title: 'Music'),
     );
   }
 }
@@ -28,84 +17,198 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  String _type = 'netease';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  TextEditingController _textController;
+
+  ScrollController _scrollController;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text("Music"),
+        trailing: CupertinoButton(
+          padding: EdgeInsets.all(0.0),
+          child: const Text('关于'),
+          onPressed: () {
+            showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (context) {
+                return CupertinoAlertDialog(
+                  title: const Text('关于本软件'),
+                  content: const Text('巴拉巴拉巴拉吧'),
+                  actions: <Widget>[
+                    CupertinoDialogAction(
+                      child: const Text('知道了'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      child: Container(
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              controller: _scrollController,
+              padding: EdgeInsets.fromLTRB(50.0, 0, 50.0, 10.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: _type == 'netease' ? 80.0 : 60.0,
+                    height: _type == 'netease' ? 80.0 : 60.0,
+                    margin: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                    ),
+                    child: GestureDetector(
+                      child: CircleAvatar(
+                        foregroundColor: Colors.blue,
+                        backgroundImage:
+                            AssetImage('assets/images/netease.jpg'),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          _type = 'netease';
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: _type == 'tencent' ? 80.0 : 60.0,
+                    height: _type == 'tencent' ? 80.0 : 60.0,
+                    margin: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                    ),
+                    child: GestureDetector(
+                      child: CircleAvatar(
+                        foregroundColor: Colors.blue,
+                        backgroundImage:
+                            AssetImage('assets/images/tencent.jpg'),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          _type = 'tencent';
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: _type == 'xiami' ? 80.0 : 60.0,
+                    height: _type == 'xiami' ? 80.0 : 60.0,
+                    margin: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                    ),
+                    child: GestureDetector(
+                      child: CircleAvatar(
+                        foregroundColor: Colors.blue,
+                        backgroundImage: AssetImage('assets/images/xiami.jpg'),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          _type = 'xiami';
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: _type == 'kugou' ? 80.0 : 60.0,
+                    height: _type == 'kugou' ? 80.0 : 60.0,
+                    margin: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                    ),
+                    child: GestureDetector(
+                      child: CircleAvatar(
+                        foregroundColor: Colors.blue,
+                        backgroundImage: AssetImage('assets/images/kugou.jpg'),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          _type = 'kugou';
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: _type == 'baidu' ? 80.0 : 60.0,
+                    height: _type == 'baidu' ? 80.0 : 60.0,
+                    margin: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                    ),
+                    child: GestureDetector(
+                      child: CircleAvatar(
+                        foregroundColor: Colors.blue,
+                        backgroundImage: AssetImage('assets/images/baidu.jpg'),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          _type = 'baidu';
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Center(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(50.0, 20.0, 50.0, 0),
+                child: CupertinoTextField(
+                  cursorColor: Colors.white,
+                  controller: _textController,
+                  placeholder: '输入歌曲名称搜索\\（￣︶￣）/',
+                  keyboardType: TextInputType.text,
+                  padding: EdgeInsets.all(20.0),
+                  textAlign: TextAlign.center,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50.0),
+                    /* border: Border.all(
+                      color: Colors.blue,
+                      style: BorderStyle.solid,
+                      width: 2.0,
+                    ), */
+                    boxShadow: const [
+                      BoxShadow(blurRadius: 20.0, color: Colors.black26),
+                    ],
+                  ),
+                  onEditingComplete: () {
+                    // TODO 打开新窗口
+                  },
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  @override
+  dispose() {
+    _textController.dispose();
+    _scrollController.dispose();
+    super.dispose();
   }
 }
